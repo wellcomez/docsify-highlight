2b3d0841b373939c4a3a3f0d0714eaeee7deb47d
zjl <zhujialai@icloud.com>
2021-03-10 19:08:35 +0800

    可以控制显示笔记

--- src/components/NoteMarker.vue
+++ src/components/NoteMarker.vue
@@ -1,21 +1,20 @@
 <template>
   <span v-on:click="onClick">
     <Tooltip
-      v-if="on"
+      v-if="hidden == false"
       :content="content"
       :max-width="200"
       :delay="500"
       class="notemarker"
       theme="light"
-      :always="on"
+      :always="always"
       placement="right"
     >
       <sup>
         <Icon type="md-create" />
       </sup>
     </Tooltip>
-
-    <sup v-if="on == false">
+    <sup v-if="hidden">
       <Icon type="md-create" />
     </sup>
   </span>
@@ -25,18 +24,41 @@ export default {
   name: "NoteMarker",
   data() {
     return {
-      on: this.always,
+      on: true,
+      always: true,
+      hidden: false,
     };
+  },
+  mounted() {
+    if (this.showall == true) {
+        this.on = true;
+    }else{
+        this.on = false;
+    }
+    this.updateWithOnStatus()
   },
   props: {
     noteid: { type: String, default: undefined },
     content: { type: String, default: "" },
-    always: { type: Boolean, default: true },
+    showall: { type: Boolean, default: undefined},
   },
   methods: {
+    updateWithOnStatus() {
+      if (this.on == false) {
+        this.hidden = true;
+        setTimeout(() => {
+          this.always = false;
+          this.hidden = false;
+        }, 100);
+      } else {
+        this.always = true;
+        this.hidden = false;
+      }
+    },
     onClick(e) {
       e.stopPropagation();
       this.on = this.on ? false : true;
+      this.updateWithOnStatus();
     },
   },
 };
