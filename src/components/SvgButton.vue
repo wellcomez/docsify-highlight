2c5a945e96e8c75cde01413e3053b5f9ea997a4c
zjl <zhujialai@icloud.com>
2021-03-12 16:12:36 +0800

    add flag

--- src/components/SvgButton.vue
+++ src/components/SvgButton.vue
@@ -1,34 +1,53 @@
 <template>
   <Tooltip :content="tips" :disabled="tipsDisabled">
-    <Button v-on:click="onClick" size="small" style="height:22px;margin:4px;">
-      <Icon  v-if="custom"  :custom="btnClass" :type="name" size="20" :color="svgcolor" style="margin-top:0px" />
-      <Icon  v-if="custom==false"  :type="name" size="20" :color="svgcolor" style="margin-top:0px" />
+    <Button v-on:click="onClick" size="small" style="height: 22px; margin: 4px">
+      <Icon
+        v-if="custom"
+        :custom="btnClass"
+        :type="name"
+        size="20"
+        :color="svgcolor"
+        style="margin-top: 0px"
+      />
+      <Icon
+        v-if="custom == false"
+        :type="name"
+        size="20"
+        :color="svgcolor"
+        style="margin-top: 0px"
+      />
     </Button>
   </Tooltip>
 </template>
 <script>
 const svgcolor_on = "#42b983";
-const svgcolor_off = ""
+const svgcolor_off = "";
 export default {
   name: "SvgButton",
   computed: {
-    tipsDisabled(){return this.tips==undefined },
+    tipsDisabled() {
+      return this.tips == undefined;
+    },
     btnClass() {
-      let ret = "iconfont "+this.name;
+      let ret = "iconfont " + this.name;
       return ret;
     },
-    customStyle(){
-      return { 
-        color:this.svgcolor,
-        height:"16px",
-        width:"16px"
-      }
+    customStyle() {
+      return {
+        color: this.svgcolor,
+        height: "16px",
+        width: "16px",
+      };
     },
     svgcolor() {
-      if (this.onOff) {
-        return this.on ? svgcolor_on : svgcolor_off;
+      let { color } = this;
+      if (color) {
+        color = svgcolor_on;
       }
-      return svgcolor_on;
+      if (this.onOff) {
+        return this.on ? color : svgcolor_off;
+      }
+      return color;
     },
   },
   data() {
@@ -36,13 +55,14 @@ export default {
   },
 
   props: {
-    custom:{type:Boolean,default:false},
+    custom: { type: Boolean, default: false },
     onClick: {
       type: Function,
       default: undefined,
     },
     name: { type: String, default: "" },
     tips: { type: String, default: undefined },
+    color: { type: String, default: undefined },
     onOff: { type: Boolean, default: undefined },
     on: { type: Boolean, default: undefined },
   },
