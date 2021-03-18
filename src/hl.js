9840b7d4632b747b23a6409e5087543710d10552
zjl <zhujialai@icloud.com>
2021-03-18 14:35:04 +0800

    add

--- src/hl.js
+++ src/hl.js
@@ -23,27 +23,41 @@ export function getIntersection(arrA, arrB) {
     arrB.forEach(i => record[i] && intersection.push(i) && (record[i] = false));
     return intersection;
 }
+function runScrip() {
+    let { DocHighlighter } = window.$docsify ? window.$docsify : undefined;
+    if (DocHighlighter) {
+        let { script } = DocHighlighter
+        if (script) {
+            try {
+                script();
+                // eslint-disable-next-line no-empty
+            } catch (error) {
+            }
+        }
+    }
+}
 function hlinit() {
-    log("hlinit-"+document.location.href)
-    if(window.hl){
+    runScrip()
+    log("hlinit-" + document.location.href)
+    if (window.hl) {
         window.hl.turnHighLight(false)
     }
     let hl = new DocHighlighter();
     window.hl = hl;
     var main = document.getElementsByClassName('content')[0]
     // eslint-disable-next-line no-unused-vars
-    let {vm} = window;
-    if(vm){
-        Vue.set(vm, "count",hl.count());
-        Vue.set(vm, "checked",hl.on());
-        Vue.set(vm, "hl",hl);
-    }else{
-        vm = window.vm = mountCmp(Panel, { checked: hl.on(),count:hl.count(),hl}, main)
+    let { vm } = window;
+    if (vm) {
+        Vue.set(vm, "count", hl.count());
+        Vue.set(vm, "checked", hl.on());
+        Vue.set(vm, "hl", hl);
+    } else {
+        vm = window.vm = mountCmp(Panel, { checked: hl.on(), count: hl.count(), hl }, main)
     }
-    hl.updatePanelCb=()=>{
+    hl.updatePanelCb = () => {
         // console.log("aaa",vm);
-        Vue.set(vm, "count",hl.count());
-    } 
+        Vue.set(vm, "count", hl.count());
+    }
 }
 export default hlinit
 
