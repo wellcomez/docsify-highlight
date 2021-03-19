e363d506d7044c7c3d424476383b1bcc17b4af95
zjl <zhujialai@icloud.com>
2021-03-19 13:12:58 +0800

    change to inline

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,31 +1,6 @@
 <template>
   <div v-if="showme" style="padding: 4px" class="login-panel">
-    <Form label-position="left" inline v-if="loginbyphone">
-      <FormItem prop="phonenumber">
-        <Input type="tel" v-model="phonenumber" placeholder="手机号">
-          <Icon type="ios-person-outline" slot="prepend"></Icon>
-        </Input>
-      </FormItem>
-      <FormItem prop="verifycode">
-        <Row>
-          <Col span="16">
-            <Input type="text" v-model="verifycode" placeholder="验证码">
-              <Icon type="ios-lock-outline" slot="prepend"></Icon>
-            </Input>
-          </Col>
-          <Col span="4" offset="1">
-            <Button @click="onRequestCode" :disabled="disableReqCode">{{
-              btn_code_title
-            }}</Button>
-          </Col>
-        </Row>
-      </FormItem>
-      <FormItem>
-        <Button type="primary" @click="onOk">Login</Button>
-        <Button style="margin-left: 8px" @click="cancel">Cancel</Button>
-      </FormItem>
-    </Form>
-    <Tabs v-model="tabname" v-else>
+    <Tabs v-model="tabname">
       <TabPane
         v-for="({ label, name }, index) in tabusername"
         :label="label"
@@ -53,10 +28,6 @@
         </Form>
       </TabPane>
     </Tabs>
-    <RadioGroup v-model="bywhat" class="radio-login-type">
-      <Radio label="byphone">手机</Radio>
-      <Radio label="byname">用户名</Radio>
-    </RadioGroup>
   </div>
 </template>
 <style>
@@ -106,49 +77,7 @@ export default {
       return User.isLogin() == false && this.open;
     },
   },
-  watch: {
-    disableReqCode(val) {
-      if (val == false) {
-        this.btn_code_title = "获取验证码";
-      }
-    },
-  },
   methods: {
-    waifforCode() {
-      this.disableReqCode = true;
-      let cout = 0;
-      let reEanble = () => {
-        cout++;
-        if (cout < 60) {
-          let left = 60 - cout;
-          this.btn_code_title = `剩余(${left})`;
-          return;
-        }
-        if (this.tm) {
-          clearInterval(this.tm);
-          this.tm = undefined;
-        }
-        this.disableReqCode = false;
-      };
-      reEanble = reEanble.bind(this);
-      this.tm = setInterval(reEanble, 1000);
-      reEanble();
-    },
-    onRequestCode() {
-      if (this.phonenumber) {
-        if (this.phonenumber[0] != "+") {
-        //   this.phonenumber = "+" + this.phonenumber;
-        }
-        User.requestcode(this.phonenumber)
-          // eslint-disable-next-line no-unused-vars
-          .then((a) => {
-            this.waifforCode();
-          })
-          .catch((e) => {
-            Message.error(e.rawMessage);
-          });
-      }
-    },
     cancel() {
       this.$emit("update:open", false);
     },
