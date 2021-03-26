e8ca8eddfc1a0abbb508559a5fc92527bb0dc9b2
zjl <zhujialai@icloud.com>
2021-03-26 16:33:52 +0800

    add

--- src/components/Bubbling.vue
+++ src/components/Bubbling.vue
@@ -1,23 +1,23 @@
 <template>
-  <Poptip theme="light" v-model="visible" width="60px">
-    <SvgButton name="ios-download" tips="Export" />
-    <!-- type="primary" -->
-    <ButtonGroup slot="content">
+  <Tooltip theme="light" :disabled="disabled" width="60px" v-click-outside="clickoutside">
+    <SvgButton name="ios-download" tips="Export" @click="disabled=disabled==false" onOff :on="disabled"/>
+    <ButtonGroup slot="content" vertical>
       <Button v-for="{ name } in list" :key="name" @click="onOption(name)">{{
         name
       }}</Button>
     </ButtonGroup>
-  </Poptip>
+  </Tooltip>
 </template>
 <script>
-// import { checkClickOut } from "../utils";
 import SvgButton from "./SvgButton";
+import ClickOutside from "vue-click-outside";
 export default {
   name: "Bubbling",
   components: { SvgButton },
+  directives:{ClickOutside},
   data() {
     return {
-      visible: false,
+      disabled: true,
       list: [
         {
           name: "json",
@@ -42,10 +42,15 @@ export default {
     },
   },
   methods: {
+    clickoutside(){
+      this.disabled = true
+    },
     onOption(name) {
+      this.disabled = true
       if (this.onSelect) {
         this.onSelect(name);
       }
+      if(this.closeButtons)
       this.closeButtons();
     },
   },
