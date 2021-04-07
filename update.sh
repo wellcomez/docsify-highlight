e9ff35e07c5c8c16cc5b2e668788b3efd825e8ab
zjl <zhujialai@icloud.com>
2021-04-07 10:24:35 +0800

    fix runscript

---
+++ update.sh
@@ -0,0 +1,2 @@
+curl https://purge.jsdelivr.net/npm/docsify-highlight@latest/dist/docsify-highlight.min.css
+curl https://purge.jsdelivr.net/npm/docsify-highlight@latest/dist/docsify-highlight.umd.min.js
