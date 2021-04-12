efadb73982ae8363e79a8e085ae3e158ac245f36
zjl <zhujialai@icloud.com>
2021-04-13 00:17:26 +0800

    beatify md

--- src/store.js
+++ src/store.js
@@ -322,16 +322,18 @@ class Chapter {
       }
       // let tile = `"${label.substring(0, Math.min(20, label.length))}..."`
       let img = ''
-      let url = this.url(id, this.rootpath);
+      let url = this.url(id);
       if (imgsrc) {
         imgsrc = getImgSrcUrl(imgsrc)
         let { path } = parseurl(imgsrc)
         img = `![${path}](${imgsrc})`
       }
       let span = label ? `<span class="${hlyellow}"> ${label}</span>` : "";
-      let tile =  img ? img : span
-      return `${idx + 1}.[^](${url})${tags}${tile}
-          
+      // let tile =  img ? img : span
+      return `${idx + 1}.[^](${url})${tags}${span}
+         
+         ${img}
+         
          ${note}
          
         `;
